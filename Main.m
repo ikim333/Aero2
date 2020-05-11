84bd5f190942a4d31a19577ab12d2b2426cbe706
Miki <38962249+ikim333@users.noreply.github.com>
2020-05-11 15:55:37 +0200

    Creat main + gitignore

---
+++ Main.m
@@ -0,0 +1,15 @@
+%% Assignment of Aerodynamics
+
+%AUTHORS: Miquel Badia, Daniel Longarón, Aranu Reyes
+
+%This program computes the lift and drag distribution along a wing using
+%the lifting line method
+
+%%
+clc;
+clear all;
+
+%% DATA INPUT
+
+
+
